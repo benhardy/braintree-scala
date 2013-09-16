2362831dfd399d92a1864434725d775969baa599
benhardy <benhardy+github@gmail.com>
2013-09-15 21:22:45 -0500

    translated CustomerRequestIT

---
+++ src/test/scala/com/braintreegateway/CustomerRequestSpec.scala
@@ -0,0 +1,35 @@
+package com.braintreegateway
+
+import _root_.org.junit.runner.RunWith
+import _root_.org.scalatest.junit.JUnitRunner
+import _root_.org.scalatest.matchers.MustMatchers
+import _root_.org.scalatest.FunSpec
+import xml.XML
+
+@RunWith(classOf[JUnitRunner])
+class CustomerRequestSpec extends FunSpec with MustMatchers {
+  describe("toXml") {
+
+    it("includes device bundle") {
+      val deviceData = "{\"device_session_id\": \"devicesession123\"}"
+      val request = new CustomerRequest().deviceData(deviceData)
+
+      val xmlString = request.toXML
+      xmlString must include ("devicesession123")
+
+      val customerNode = XML.loadString(xmlString)
+      (customerNode \ "deviceData").head.text must be === deviceData
+    }
+
+    it("includes Security Params") {
+      val deviceSessionId = "devicesession123"
+      val request = new CustomerRequest().creditCard.deviceSessionId(deviceSessionId).done
+
+      val xmlString = request.toXML
+      xmlString must include (deviceSessionId)
+
+      val customerNode = XML.loadString(xmlString)
+      (customerNode \ "creditCard" \ "deviceSessionId").head.text must be === deviceSessionId
+    }
+  }
+}
