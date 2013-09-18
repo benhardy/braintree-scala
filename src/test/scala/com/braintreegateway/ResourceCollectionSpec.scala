c73b8b3d143568546fdc433015142f95234939ae
benhardy <benhardy+github@gmail.com>
2013-09-18 13:26:28 -0500

    translated ResourceCollection test

---
+++ src/test/scala/com/braintreegateway/ResourceCollectionSpec.scala
@@ -0,0 +1,41 @@
+package com.braintreegateway
+
+import _root_.org.junit.runner.RunWith
+import _root_.org.scalatest.FunSpec
+import _root_.org.scalatest.junit.JUnitRunner
+import _root_.org.scalatest.matchers.MustMatchers
+import com.braintreegateway.util.NodeWrapperFactory
+import java.util.List
+import scala.collection.JavaConversions._
+
+@RunWith(classOf[JUnitRunner])
+class ResourceCollectionSpec extends FunSpec with MustMatchers {
+  describe("getFirst") {
+    it("produces expected count of resources") {
+      val resultXml = <search-results>
+        <page-size>2</page-size>
+        <ids type="array">
+          <items>0</items> <items>1</items> <items>2</items> <items>3</items> <items>4</items>
+        </ids>
+      </search-results>
+
+      val rootNode = NodeWrapperFactory.instance.create(resultXml.toString)
+
+      val resourceCollection = new ResourceCollection[String](new TestPager, rootNode)
+
+      val items = resourceCollection.toList
+      items.length must be === (values.length)
+
+      items must be === (values.toList)
+    }
+  }
+
+  private val values = Array("a", "b", "c", "d", "e")
+
+  private[braintreegateway] class TestPager extends Pager[String] {
+    def getPage(ids: java.util.List[String]) = {
+      ids.map { s => values(Integer.parseInt(s)) }
+    }
+  }
+
+}
